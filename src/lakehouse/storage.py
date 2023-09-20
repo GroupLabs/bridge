842470797e75b3aacf599c8cd0c5ff50d2275b5f
Noel Fran Thomas <54130980+noelfranthomas@users.noreply.github.com>
2023-09-19 22:32:17 -0600

    vector store can handle basic objects

--- src/lakehouse/storage.py
+++ src/lakehouse/storage.py
@@ -4,8 +4,10 @@ import csv
 import os
 
 from knowledge_graph import Graph
+from vector_store import VectorStore
 
 kg = Graph("bolt://localhost:7687", "neo4j", "eternal-pyramid-corner-jester-bread-6973")
+vs = VectorStore()
 
 from enum import Enum
 
@@ -22,15 +24,27 @@ class AllowedExtensions(Enum):
 
 class Metadata:
     def __init__(self):
+        self._name = None
         self._extension = None
         self._is_structured = None
+        self._description = None
         
     def __repr__(self) -> str:
         r = ''
+        r = r + f"name: {self._name}\n"
         r = r + f"ext: {self._extension}\n"
-        r = r + f"structured: {self._is_structured}"
+        r = r + f"structured: {self._is_structured}\n"
+        r = r + f"description: {self._description}"
         
         return r
+    
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        self._name = value
 
     @property
     def extension(self):
@@ -50,6 +64,24 @@ class Metadata:
     @is_structured.setter
     def is_structured(self, value: bool):
         self._is_structured = value
+        
+        
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, value: str):
+        self._description = value    
+    
+    def as_object(self):
+        obj = {}
+        obj["name"] = self._name
+        obj["extension"] = self._extension
+        obj["structured"] = self._is_structured
+        obj["description"] = self._description
+        
+        return obj
 
 
 def metadata(input):
@@ -58,7 +90,13 @@ def metadata(input):
     
     if isinstance(input, str):
         if os.path.exists(input):
-            _, ext = os.path.splitext(input)
+            name, ext = os.path.splitext(input)
+            
+            metadata.name = name
+            
+            # get description from desc file
+            with open(f'{name}.desc', 'r') as f:
+                metadata.description = f.read()
             
             # structured
             if ext.lower() == '.csv':
@@ -88,13 +126,26 @@ def metadata(input):
 def store(input):
     
     # generate file meta (struct/unstruct)
+    meta = metadata(input)
+    
     # store node (name, desc; meta: ...)
+    
+    
     # store vec (desc; meta: document name)
+    vs.store_object(meta.as_object())
     
-    pass
-    
-    
-
 if __name__ == "__main__":
-    m = metadata("hello.txt")
-    print(m)
+    # m = metadata("hello.txt")
+    # print(m)
+    
+    # print(m.as_object())
+    
+    store("../../data/hello.txt")
+    
+    
+    
+    l = vs.values
+    
+    print(l)
+    
+    vs.save_index('test')  # always overwrites test
