d67cb341a1902f9ea6356d61406914a2e999ae4d
Benjamin LeBlanc <bleblanc1904@gmail.com>
2023-09-23 17:48:49 -0700

    added CSV functions, added graph functionality

--- src/lakehouse/storage.py
+++ src/lakehouse/storage.py
@@ -2,14 +2,14 @@
 
 import csv
 import os
-from dotenv import load_dotenv
-
-from knowledge_graph import Graph
+from dotenv import load_dotenv, find_dotenv
+import pandas as pd
+from enum import Enum
+from graph import Graph
 from vector_store import VectorStore
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
-from enum import Enum
 
 class AllowedExtensions(Enum):
     
@@ -28,13 +28,15 @@ class Metadata:
         self._extension = None
         self._is_structured = None
         self._description = None
+        self._contents = None
         
     def __repr__(self) -> str:
         r = ''
         r = r + f"name: {self._name}\n"
         r = r + f"ext: {self._extension}\n"
         r = r + f"structured: {self._is_structured}\n"
-        r = r + f"description: {self._description}"
+        r = r + f"description: {self._description}\n"
+        r = r + f"contents: {self._contents}"
         
         return r
     
@@ -73,6 +75,16 @@ class Metadata:
     @description.setter
     def description(self, value: str):
         self._description = value    
+        
+    @property
+    def contents(self):
+        return self._contents
+
+    @contents.setter
+    def contents(self, value):
+        self._contents = value    
+        
+        
     
     def as_object(self):
         obj = {}
@@ -80,8 +92,20 @@ class Metadata:
         obj["extension"] = self._extension
         obj["structured"] = self._is_structured
         obj["description"] = self._description
+        obj["contents"] = self._contents
         
         return obj
+    
+    def csv_extraction(self, name, input):
+        df_dict = {}
+        df = pd.read_csv(input, encoding = 'latin-1')
+        df_keys = []
+        for column in df.columns.tolist():
+            if ("id") in column.lower():
+                df_keys.append(column)
+        df_dict[name] = {"columns" : df.columns.tolist(),
+                "keys": df_keys}
+        return df_dict
 
 
 def metadata(input):
@@ -90,19 +114,27 @@ def metadata(input):
     
     if isinstance(input, str):
         if os.path.exists(input):
-            name, ext = os.path.splitext(input)
+            file_name = os.path.basename(input) # Gets rid of file path if a file path is used
+            name, ext = os.path.splitext(file_name)
             
             metadata.name = name
             
             # get description from desc file
-            # This doesn't seem to work for now, takes all file and assuems they are .desc
-            with open(f'{name}.desc', 'r') as f:
-                metadata.description = f.read()
+            # This doesn't seem to work for now, takes all file and assumes they are .desc
+            if os.path.exists(f'{name}.desc'):
+                with open(f'{name}.desc', 'r') as f:
+                    metadata.description = f.read()
+            else: 
+                print(f"No description file provided for {name}{ext}")
+                metadata.description = "No description file provided"
             
             # structured
             if ext.lower() == '.csv':
                 metadata.extension = "CSV"
                 metadata.is_structured = True
+                df_dict = metadata.csv_extraction(name, input)
+                metadata.contents = df_dict
+                ## Add columns variable to metadata for csvs
             elif ext.lower() == '.tsv':
                 metadata.extension = "TSV"
                 metadata.is_structured = True
@@ -150,18 +182,61 @@ class Storage:
     def store(self, input):
         # generate file meta (struct/unstruct)
         meta = metadata(input)
-        
+        name = meta.name
+        description = meta.description
         # store node (name, desc; meta: ...)
+        # self.graph.storenode
+        # A function that stores
+        if meta.is_structured:
+            primary_keys = meta.contents.get(name).get("keys")
+            columns = meta.contents.get(name).get("columns")
+            self.graph.add_table_node(name, primary_keys, columns, labels="TABLE")
+        else:
+            self.graph.add_basic_node(name, description, labels="UNSTRUCTURED")
         
         # store vec (desc; meta: document name)
-        self.vs.store_object(meta.as_object())
+        # self.vs.store_object(meta.as_object())
         
     def save(self, name):
         # save/close conn of graph
+        self.graph.close()
         
         # save index and values
-        self.vs.save(name)
-
+        # self.vs.save(name)
+        
+    # Store a list of inputs
+    def store_list(self, list: list):
+        for input in list:
+            self.store(input)
+    
+    def create_relationship(self, name1, name2, relation):
+        self.graph.add_bidirectional_relationship(name1, name2, relation)
+    
+    def create_relationships_structured(self, input_list: list):
+        df_dict = {}
+        for input in input_list:
+            meta = metadata(input)
+            if not meta.is_structured:
+                print("This function is for structured data!")
+                return
+            name = meta.name
+            df_dict[name] = meta.contents.get(name)
+        table_relationships = []
+        for table1, dict1 in df_dict.items():
+            keys1 = dict1.get("keys")
+            for table2, dict2 in df_dict.items():
+                keys2 = dict2.get("keys")
+                if table1 != table2:
+                    shared_keys = list(set(keys1) & set(keys2))
+                    if shared_keys:
+                        for i, shared_key in enumerate(shared_keys, start = 1):
+                            table_relationships.append((table1, table2, f"{table1}.{shared_key} = {table2}.{shared_key}", i))
+        for relationship in table_relationships:
+            node_one = relationship[0]
+            node_two = relationship[1] 
+            key = f"""JOIN {{key : "{relationship[2]}"}}"""
+            # print(node_one, node_two, key)
+            self.graph.add_relationship(name_node_one= node_one, name_node_two= node_two, relation_name= key)
 
 if __name__ == "__main__":
     storage = Storage()
