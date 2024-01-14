173cdad9390a61f183886203cb544f50e1c5fc0e
Noel Thomas <noelthomas@Noels-MacBook-Pro.local>
2024-01-14 00:07:11 -0700

    demo v1

--- ml_pipelines/tressl/demo.py
+++ ml_pipelines/tressl/demo.py
@@ -1,81 +1,95 @@
-# to run first:
-# pip install easyocr marker
-
 import streamlit as st
+import os
+from PIL import Image
+import pandas as pd
+import shutil
 
-st.title("Data Ingestor (Human in the Loop)")
+from demoutils import save_uploaded_file, process_pdf_tatr, pdf_to_image_per_page
+
+if 'dfs' not in st.session_state:
+    st.session_state['dfs'] = []
+
+temp_folder = 'temp'
+temp_created_flag = False
+
+def remove_temp(temp_folder=temp_folder):
+    try:
+        shutil.rmtree(temp_folder)
+        print(f"Directory '{temp_folder}' has been deleted successfully.")
+    except OSError as e:
+        print(f"Error: {e.strerror}")
 
 with st.sidebar:
-    data = st.file_uploader("Data (PDF, JPEG)", label_visibility='visible')
-
-    table_of_qs = st.file_uploader("Output (CSV, XLSX)", label_visibility='visible')
-
-if table_of_qs is not None:
-    st.write(table_of_qs)
-
-import tempfile
-import shutil
-import PyPDF2
-import os
-
-from marker.convert import convert_single_pdf
-from marker.models import load_all_models
-import json
-
-fname = PDF_path
-model_lst = load_all_models()
-
-print("models loaded")
-
-from vector_store import VectorStore
-
-vs = VectorStore()
-
-def marker(pdf_path):
-    full_text, out_meta = convert_single_pdf(pdf_path, model_lst, max_pages=None, parallel_factor=2)
-
-    out_path = f'{pdf_path}_marked.md'
-
-    with open(out_path, "w+", encoding='utf-8') as f:
-        f.write(full_text)
-
-    out_meta_filename = out_path.rsplit(".", 1)[0] + "_meta.json"
-    with open(out_meta_filename, "w+") as f:
-        f.write(json.dumps(out_meta, indent=4))
-
-    return full_text
-
-def split_pdf_into_pages(pdf_path, output_folder):
-    with open(pdf_path, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
-
-        for page_num in range(len(reader.pages)):
-            writer = PyPDF2.PdfWriter()
-            writer.add_page(reader.pages[page_num])
-
-            output_filename = os.path.join(output_folder, f'page_{page_num + 1}.pdf')
-            with open(output_filename, 'wb') as output_file:
-                writer.write(output_file)
-
-            marked_text = marker(output_filename)
-            vs.store_page(marked_text, page_num + 1)
-            print(f"Ingested: {output_filename}")
-
-def save_uploaded_file(uploaded_file):
-    try:
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
-            shutil.copyfileobj(uploaded_file, tmp_file)
-            return tmp_file.name
-    except Exception as e:
-        print(e)
-        return None
+    st.title("Data Ingestor")
+    st.subheader("(Human in the Loop)")
+    st.divider()
+    data = st.file_uploader("Data", label_visibility='collapsed', type='pdf', on_change=remove_temp)
 
 if data is not None:
     temp_file_path = save_uploaded_file(data)
-    if temp_file_path:
-        split_pdf_into_pages(temp_file_path, 'temp')
-        # Remember to delete the temp file after processing
-        os.remove(temp_file_path)
+    temp_created_flag = pdf_to_image_per_page(temp_file_path, 'temp')
+
+def load_images_from_folder(folder):
+    # Get all file names and sort them
+    filenames = sorted([f for f in os.listdir(folder) if f.endswith((".png", ".jpg", ".jpeg"))])
+    
+    for filename in filenames:
+        yield os.path.join(folder, filename)
+
+
+if temp_created_flag:
+    # Load images
+    images = list(load_images_from_folder(temp_folder))
+
+    # Create tabs for each image
+    tab_titles = [f"Page {i+1}" for i in range(len(images))]
+    tabs = st.tabs(tab_titles)
+
+    for tab, image_path in zip(tabs, images):
+        with tab:
+
+            image_cont = st.empty()
+
+            image = Image.open(image_path)
+
+            image_cont.image(image, caption=os.path.basename(image_path))
+            
+            try:
+                tbimage, tbdf, tbdata = process_pdf_tatr(image)
+
+                st.session_state['dfs'].append(tbdf)
+
+                with image_cont.container():
+                    col1, col2 = st.columns(2)
+
+                    st.data_editor(tbdf)
+
+                    with col1:
+                        st.image(image, caption=os.path.basename(image_path))
+
+                    with col2:
+                        st.image(tbimage)
+            
+            except IndexError:
+                st.info("No table found")
+
+
+@st.cache_resource
+def convert_df(dfs):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+
+    df = pd.concat(dfs)
+
+    return df.to_csv().encode('utf-8')
+
+with st.sidebar:
+    if len(st.session_state['dfs']) > 0:
+
+        st.toast("Ready to download")
+
+        st.download_button(
+            label="Download data as CSV",
+            data=convert_df(st.session_state['dfs']),
+            file_name=f'{data.name}.csv',
+            mime='text/csv',
+        )
