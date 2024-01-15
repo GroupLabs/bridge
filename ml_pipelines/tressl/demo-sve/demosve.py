920e380b1f92b4f056022895d586cebb08f677d0
Noel Thomas <noelthomas@Noels-MacBook-Pro.local>
2024-01-14 20:13:28 -0700

    refactor

---
+++ ml_pipelines/tressl/demo-sve/demosve.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+from demoutilssve import save_uploaded_file, split_pdf_into_pages, docvqa_pipeline
+
+st.title("Data Ingestor (Human in the Loop)")
+
+with st.sidebar:
+    data = st.file_uploader("Data (PDF, JPEG)", label_visibility='visible')
+
+if data is not None:
+    temp_file_path = save_uploaded_file(data)
+    split_pdf_into_pages(temp_file_path, 'temp')
+
+    query = st.text_input("Question")
+
+    if query:
+        ans, page_image = docvqa_pipeline(query)
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.write(ans[0]['answer'])
+
+        with col2:
+            st.image(page_image)
---
+++ ml_pipelines/tressl/demo-sve/demoutilssve.py
@@ -0,0 +1,461 @@
+import tempfile
+import shutil
+import PyPDF2
+import os
+
+from marker.convert import convert_single_pdf
+from marker.models import load_all_models
+import json
+
+import streamlit as st
+
+@st.cache_data
+def save_uploaded_file(uploaded_file):
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+            shutil.copyfileobj(uploaded_file, tmp_file)
+            return tmp_file.name
+    except Exception as e:
+        print(e)
+        return None
+
+@st.cache_resource
+def load_models():
+    return load_all_models()
+
+model_lst = load_models()
+
+from vector_store import VectorStore
+
+vs = VectorStore()
+
+def marker(pdf_path):
+    full_text, out_meta = convert_single_pdf(pdf_path, model_lst, max_pages=None, parallel_factor=2)
+
+    out_path = f'{pdf_path}_marked.md'
+
+    with open(out_path, "w+", encoding='utf-8') as f:
+        f.write(full_text)
+
+    out_meta_filename = out_path.rsplit(".", 1)[0] + "_meta.json"
+    with open(out_meta_filename, "w+") as f:
+        f.write(json.dumps(out_meta, indent=4))
+
+    return full_text
+
+@st.cache_data
+def split_pdf_into_pages(pdf_path, output_folder):
+    ingest_progress = st.progress(0, text="Ingesting PDF")
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+
+        for page_num in range(len(reader.pages)):
+            writer = PyPDF2.PdfWriter()
+            writer.add_page(reader.pages[page_num])
+
+            output_filename = os.path.join(output_folder, f'page_{page_num + 1}.pdf')
+            with open(output_filename, 'wb') as output_file:
+                writer.write(output_file)
+
+            marked_text = marker(output_filename)
+            vs.store_page(marked_text, page_num + 1)
+            print(f"Ingested: {output_filename}")
+
+            ingest_progress.progress(page_num / len(reader.pages), f"Ingested page: {page_num}")
+
+    ingest_progress.empty()
+
+from pdf2image import convert_from_path
+from PIL import Image
+
+def pdf_to_image(pdf_path):
+    images = convert_from_path(pdf_path)
+
+    # Calculate the total height of all images (assuming they have the same width)
+    total_height = sum(img.size[1] for img in images)
+    max_width = max(img.size[0] for img in images)
+
+    # Create a new image with the appropriate height and width
+    combined_image = Image.new('RGB', (max_width, total_height))
+
+    # Paste the images together
+    y_offset = 0
+    for img in images:
+        combined_image.paste(img, (0, y_offset))
+        y_offset += img.size[1]
+
+    return combined_image
+
+from transformers import pipeline
+
+nlp = pipeline(
+    "document-question-answering",
+    model="impira/layoutlm-document-qa",
+)
+
+# nlp = pipeline(
+#     "document-question-answering", 
+#     model="impira/layoutlm-invoices"
+# )
+
+# nlp = pipeline("document-question-answering", model="naver-clova-ix/donut-base-finetuned-docvqa")
+
+def docvqa_pipeline(query):
+    res_obj = vs.query(query)
+
+    print(res_obj)
+
+    img = pdf_to_image(f"temp/page_{res_obj[0][0]}.pdf")
+
+    return nlp(img, query), img
+
+
+
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.patches import Patch
+import io
+from PIL import Image, ImageDraw
+import numpy as np
+import csv
+import pandas as pd
+
+from torchvision import transforms
+
+from transformers import AutoModelForObjectDetection
+import torch
+
+import easyocr
+
+import gradio as gr
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+class MaxResize(object):
+    def __init__(self, max_size=800):
+        self.max_size = max_size
+
+    def __call__(self, image):
+        width, height = image.size
+        current_max_size = max(width, height)
+        scale = self.max_size / current_max_size
+        resized_image = image.resize(
+            (int(round(scale * width)), int(round(scale * height)))
+        )
+
+        return resized_image
+
+
+detection_transform = transforms.Compose(
+    [
+        MaxResize(800),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ]
+)
+
+structure_transform = transforms.Compose(
+    [
+        MaxResize(1000),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ]
+)
+
+# load table detection model
+# processor = TableTransformerImageProcessor(max_size=800)
+model = AutoModelForObjectDetection.from_pretrained(
+    "microsoft/table-transformer-detection", revision="no_timm"
+).to(device)
+
+# load table structure recognition model
+# structure_processor = TableTransformerImageProcessor(max_size=1000)
+structure_model = AutoModelForObjectDetection.from_pretrained(
+    "microsoft/table-transformer-structure-recognition-v1.1-all"
+).to(device)
+
+# load EasyOCR reader
+reader = easyocr.Reader(["en"])
+
+
+# for output bounding box post-processing
+def box_cxcywh_to_xyxy(x):
+    x_c, y_c, w, h = x.unbind(-1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h), (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return torch.stack(b, dim=1)
+
+
+def rescale_bboxes(out_bbox, size):
+    width, height = size
+    boxes = box_cxcywh_to_xyxy(out_bbox)
+    boxes = boxes * torch.tensor([width, height, width, height], dtype=torch.float32)
+    return boxes
+
+
+def outputs_to_objects(outputs, img_size, id2label):
+    m = outputs.logits.softmax(-1).max(-1)
+    pred_labels = list(m.indices.detach().cpu().numpy())[0]
+    pred_scores = list(m.values.detach().cpu().numpy())[0]
+    pred_bboxes = outputs["pred_boxes"].detach().cpu()[0]
+    pred_bboxes = [elem.tolist() for elem in rescale_bboxes(pred_bboxes, img_size)]
+
+    objects = []
+    for label, score, bbox in zip(pred_labels, pred_scores, pred_bboxes):
+        class_label = id2label[int(label)]
+        if not class_label == "no object":
+            objects.append(
+                {
+                    "label": class_label,
+                    "score": float(score),
+                    "bbox": [float(elem) for elem in bbox],
+                }
+            )
+
+    return objects
+
+
+def fig2img(fig):
+    """Convert a Matplotlib figure to a PIL Image and return it"""
+    buf = io.BytesIO()
+    fig.savefig(buf)
+    buf.seek(0)
+    image = Image.open(buf)
+    return image
+
+
+def visualize_detected_tables(img, det_tables):
+    plt.imshow(img, interpolation="lanczos")
+    fig = plt.gcf()
+    fig.set_size_inches(20, 20)
+    ax = plt.gca()
+
+    for det_table in det_tables:
+        bbox = det_table["bbox"]
+
+        if det_table["label"] == "table":
+            facecolor = (1, 0, 0.45)
+            edgecolor = (1, 0, 0.45)
+            alpha = 0.3
+            linewidth = 2
+            hatch = "//////"
+        elif det_table["label"] == "table rotated":
+            facecolor = (0.95, 0.6, 0.1)
+            edgecolor = (0.95, 0.6, 0.1)
+            alpha = 0.3
+            linewidth = 2
+            hatch = "//////"
+        else:
+            continue
+
+        rect = patches.Rectangle(
+            bbox[:2],
+            bbox[2] - bbox[0],
+            bbox[3] - bbox[1],
+            linewidth=linewidth,
+            edgecolor="none",
+            facecolor=facecolor,
+            alpha=0.1,
+        )
+        ax.add_patch(rect)
+        rect = patches.Rectangle(
+            bbox[:2],
+            bbox[2] - bbox[0],
+            bbox[3] - bbox[1],
+            linewidth=linewidth,
+            edgecolor=edgecolor,
+            facecolor="none",
+            linestyle="-",
+            alpha=alpha,
+        )
+        ax.add_patch(rect)
+        rect = patches.Rectangle(
+            bbox[:2],
+            bbox[2] - bbox[0],
+            bbox[3] - bbox[1],
+            linewidth=0,
+            edgecolor=edgecolor,
+            facecolor="none",
+            linestyle="-",
+            hatch=hatch,
+            alpha=0.2,
+        )
+        ax.add_patch(rect)
+
+    plt.xticks([], [])
+    plt.yticks([], [])
+
+    legend_elements = [
+        Patch(
+            facecolor=(1, 0, 0.45),
+            edgecolor=(1, 0, 0.45),
+            label="Table",
+            hatch="//////",
+            alpha=0.3,
+        ),
+        Patch(
+            facecolor=(0.95, 0.6, 0.1),
+            edgecolor=(0.95, 0.6, 0.1),
+            label="Table (rotated)",
+            hatch="//////",
+            alpha=0.3,
+        ),
+    ]
+    plt.legend(
+        handles=legend_elements,
+        bbox_to_anchor=(0.5, -0.02),
+        loc="upper center",
+        borderaxespad=0,
+        fontsize=10,
+        ncol=2,
+    )
+    plt.gcf().set_size_inches(10, 10)
+    plt.axis("off")
+
+    return fig
+
+
+def detect_and_crop_table(image):
+    # prepare image for the model
+    # pixel_values = processor(image, return_tensors="pt").pixel_values
+    pixel_values = detection_transform(image).unsqueeze(0).to(device)
+
+    # forward pass
+    with torch.no_grad():
+        outputs = model(pixel_values)
+
+    # postprocess to get detected tables
+    id2label = model.config.id2label
+    id2label[len(model.config.id2label)] = "no object"
+    detected_tables = outputs_to_objects(outputs, image.size, id2label)
+
+    # visualize
+    # fig = visualize_detected_tables(image, detected_tables)
+    # image = fig2img(fig)
+
+    # crop first detected table out of image
+    cropped_table = image.crop(detected_tables[0]["bbox"])
+
+    return cropped_table
+
+
+def recognize_table(image):
+    # prepare image for the model
+    # pixel_values = structure_processor(images=image, return_tensors="pt").pixel_values
+    pixel_values = structure_transform(image).unsqueeze(0).to(device)
+
+    # forward pass
+    with torch.no_grad():
+        outputs = structure_model(pixel_values)
+
+    # postprocess to get individual elements
+    id2label = structure_model.config.id2label
+    id2label[len(structure_model.config.id2label)] = "no object"
+    cells = outputs_to_objects(outputs, image.size, id2label)
+
+    # visualize cells on cropped table
+    draw = ImageDraw.Draw(image)
+
+    for cell in cells:
+        draw.rectangle(cell["bbox"], outline="red")
+
+    return image, cells
+
+
+def get_cell_coordinates_by_row(table_data):
+    # Extract rows and columns
+    rows = [entry for entry in table_data if entry["label"] == "table row"]
+    columns = [entry for entry in table_data if entry["label"] == "table column"]
+
+    # Sort rows and columns by their Y and X coordinates, respectively
+    rows.sort(key=lambda x: x["bbox"][1])
+    columns.sort(key=lambda x: x["bbox"][0])
+
+    # Function to find cell coordinates
+    def find_cell_coordinates(row, column):
+        cell_bbox = [
+            column["bbox"][0],
+            row["bbox"][1],
+            column["bbox"][2],
+            row["bbox"][3],
+        ]
+        return cell_bbox
+
+    # Generate cell coordinates and count cells in each row
+    cell_coordinates = []
+
+    for row in rows:
+        row_cells = []
+        for column in columns:
+            cell_bbox = find_cell_coordinates(row, column)
+            row_cells.append({"column": column["bbox"], "cell": cell_bbox})
+
+        # Sort cells in the row by X coordinate
+        row_cells.sort(key=lambda x: x["column"][0])
+
+        # Append row information to cell_coordinates
+        cell_coordinates.append(
+            {"row": row["bbox"], "cells": row_cells, "cell_count": len(row_cells)}
+        )
+
+    # Sort rows from top to bottom
+    cell_coordinates.sort(key=lambda x: x["row"][1])
+
+    return cell_coordinates
+
+
+def apply_ocr(cell_coordinates, cropped_table):
+    # let's OCR row by row
+    data = dict()
+    max_num_columns = 0
+    for idx, row in enumerate(cell_coordinates):
+        row_text = []
+        for cell in row["cells"]:
+            # crop cell out of image
+            cell_image = np.array(cropped_table.crop(cell["cell"]))
+            # apply OCR
+            result = reader.readtext(np.array(cell_image))
+            if len(result) > 0:
+                text = " ".join([x[1] for x in result])
+                row_text.append(text)
+
+        if len(row_text) > max_num_columns:
+            max_num_columns = len(row_text)
+
+        data[str(idx)] = row_text
+
+    # pad rows which don't have max_num_columns elements
+    # to make sure all rows have the same number of columns
+    for idx, row_data in data.copy().items():
+        if len(row_data) != max_num_columns:
+            row_data = row_data + ["" for _ in range(max_num_columns - len(row_data))]
+        data[str(idx)] = row_data
+
+    # write to csv
+    with open("output.csv", "w") as result_file:
+        wr = csv.writer(result_file, dialect="excel")
+
+        for row, row_text in data.items():
+            wr.writerow(row_text)
+
+    # return as Pandas dataframe
+    df = pd.read_csv("output.csv")
+
+    return df, data
+
+
+def process_pdf_tatr(image):
+    cropped_table = detect_and_crop_table(image)
+
+    image, cells = recognize_table(cropped_table)
+
+    cell_coordinates = get_cell_coordinates_by_row(cells)
+
+    df, data = apply_ocr(cell_coordinates, image)
+
+    return image, df, data
