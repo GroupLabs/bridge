f4ae91af179edecf273b4816177a08f3e2e98016
Noel Thomas <noelthomas@Noels-MacBook-Pro.local>
2024-04-08 00:14:42 -0600

    hacky code

--- src/frontend/components/messages/message-markdown.tsx
+++ src/frontend/components/messages/message-markdown.tsx
@@ -9,6 +9,42 @@ interface MessageMarkdownProps {
 }
 
 export const MessageMarkdown: FC<MessageMarkdownProps> = ({ content }) => {
+  const parseJson = (str: string) => {
+    try {
+      return JSON.parse(str);
+    } catch (e) {
+      return null;
+    }
+  };
+
+  const jsonContent = parseJson(content);
+
+  if (jsonContent) {
+    return (
+      <div>
+      {jsonContent.resp.map(([id, details]: [string, { score: number; text: string }]) => (
+        <div key={id} className="border-b border-gray-200 bg-white rounded-lg px-4 py-5 sm:px-6 my-3">
+          <div className="-ml-4 -mt-4 flex flex-wrap items-center justify-between sm:flex-nowrap">
+            <div className="ml-4 mt-4">
+
+              <div className="flex flex-wrap items-center justify-between">
+                <h3 className="text-base font-semibold leading-6 text-gray-900 py-2">{id}</h3>
+                <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+                  {Number(details.score).toFixed(4)}
+                </span>
+              </div>
+              <p className="mt-1 text-sm text-gray-500 py-2">
+                {details.text.length > 400 ? `${details.text.slice(0, 400)}...` : details.text}
+              </p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+    );
+  }
+
+  
   return (
     <MessageMarkdownMemoized
       className="prose dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 min-w-full space-y-6 break-words"
