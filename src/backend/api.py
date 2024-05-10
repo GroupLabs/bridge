57eac985d06a214f56274e2e2a865c392fdaf88c
Damian Loch <163156541+damianloch@users.noreply.github.com>
2024-05-09 21:54:49 -0400

    got rid of ollama chat

--- src/backend/api.py
+++ src/backend/api.py
@@ -12,7 +12,7 @@ from storage import load_data, load_model, query
 from serverutils import Health, Status, Load
 from serverutils import Query
 from serverutils import ChatRequest
-from ollama import chat
+#from ollama import chat
 
 TEMP_DIR = "./temp"
 
