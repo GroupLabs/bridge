7ddb003f4abdcff89d9623cfd8f2c7c2da5495d9
codyc-f <codycf2003@gmail.com>
2024-06-05 16:18:07 -0400

    Update api.py

--- src/backend/api.py
+++ src/backend/api.py
@@ -184,8 +184,8 @@ async def nl_query(input: Query):
 
     return {"health": health, "status" : "success", "resp" : resp}
 
-@app.post("/sort")
-async def sort_docs_ep(type: str=Form(...)):
+async def sort_docs_ep(type: str = Form(...)):
+    global last_sort_type, last_sort_order
     type_of_sort = ["name", "size", "type", "created"]
     if type not in type_of_sort:
         return "invalid sort"
