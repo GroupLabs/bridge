feef68461c2f6f7fc712a6f35d53ef4d82df98d1
damianloch <damian.loch@arvoai.ca>
2024-06-06 16:09:15 -0400

    updated gen to gen2

--- src/backend/auto_description.py
+++ src/backend/auto_description.py
@@ -2,7 +2,7 @@ from ollama import gen2, chat_with_model_to_get_description
 
 def describe_table(input):
 
-    resp = gen(f"The following is a table. Look at the columns and infer what the table is about. Return just the description - as detailed as possible, but keep it straight to the point: {input}")
+    resp = gen2(f"The following is a table. Look at the columns and infer what the table is about. Return just the description - as detailed as possible, but keep it straight to the point: {input}")
 
     return resp
 
