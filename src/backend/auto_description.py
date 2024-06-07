a2d0166dfe452ae71ea0c5342627acefa7c74892
codyc-f <codycf2003@gmail.com>
2024-06-07 10:28:11 -0400

    Revert "removed streaming for describe_table"
    This reverts commit dc55f28c79361e53d37a80a293c84ae2b7a3f6af.

--- src/backend/auto_description.py
+++ src/backend/auto_description.py
@@ -1,8 +1,8 @@
-from ollama import gen1, chat_with_model_to_get_description
+from ollama import gen2, chat_with_model_to_get_description
 
 def describe_table(input):
 
-    resp = gen1(f"The following is a table. Look at the columns and infer what the table is about. Return just the description - as detailed as possible, but keep it straight to the point: {input}")
+    resp = gen2(f"The following is a table. Look at the columns and infer what the table is about. Return just the description - as detailed as possible, but keep it straight to the point: {input}")
 
     return resp
 
