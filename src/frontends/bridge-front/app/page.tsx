12642afeb2b17a8e7f50b865e460919edc5960c7
damianloch <damian.loch@arvoai.ca>
2024-07-09 16:42:31 -0400

    Clerk kinda works I think

--- src/frontends/bridge-front/app/page.tsx
+++ src/frontends/bridge-front/app/page.tsx
@@ -1,14 +1,21 @@
 import { Chat } from '@/components/chat'
 import { nanoid } from 'ai'
 import { AI } from './actions'
+import Header from '@/components/header'
+
 
 export const maxDuration = 60
 
 export default function Page() {
   const id = nanoid()
   return (
-    <AI initialAIState={{ chatId: id, messages: [] }}>
+    <div>
+           <Header /> 
+
+<AI initialAIState={{ chatId: id, messages: [] }}>
       <Chat id={id} />
     </AI>
+    </div>
+    
   )
 }
--- src/frontends/bridge-front/app/sign-up/[[...sign-up]]/layout.tsx
+++ src/frontends/bridge-front/app/sign-up/[[...sign-up]]/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata, Viewport } from 'next'
 import { Inter as FontSans } from 'next/font/google'
-import './globals.css'
 import { cn } from '@/lib/utils'
 import { ThemeProvider } from '@/components/theme-provider'
+import Header from '@/components/header'
 import Footer from '@/components/footer'
 import { Sidebar } from '@/components/sidebar'
 import { Toaster } from '@/components/ui/sonner'
@@ -46,25 +46,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <ClerkProvider>
-    <html lang="en" suppressHydrationWarning>
-      <body className={cn('font-sans antialiased', fontSans.variable)}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-
-        >
-          <SignOutButton/>
+    <div className='flex items-center justify-center'>
           {children}
-          {/* <Sidebar /> */}
-          <Footer />
-          <Toaster />
-        </ThemeProvider>
-      </body>
-    </html>
-    </ClerkProvider>
+
+    </div>
+          
     
   )
 }
